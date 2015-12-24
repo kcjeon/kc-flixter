23aa4a2fdce52a615616a2f443abbc41fcef920a
vagrant <you@example.comwritercassiejeon@gmail.com>
2015-12-24 19:36:14 +0000

    Ready for commit

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,6 +1,9 @@
 class Section < ActiveRecord::Base
   belongs_to :course
   has_many :lessons
+
+  include RankedModel
+  ranks :row_order, :with_same => :course_id
 end
 
 # Start at "Let's add the relationship"
